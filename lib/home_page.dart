285765bb5bd54a8b9ca4c2e1f6862f99c23f3e8f
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2023-03-03 12:44:34 -0300

    Tela principal pronta!

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:calca_imc/setState/imc_setstate_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => page,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => _goToPage(
+                context,
+                const ImcSetstatePage(),
+              ),
+              child: const Text('SetState'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Value Notifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Change Notifier'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Bloc PAttern (Streams)'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
